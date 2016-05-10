4fc676dc455baca73d252008f2ca18147d99177f
Dom3k <ja@dom3k.pl>
2016-05-10 15:43:07 +0200

    Initial commit; source to be worked on!

---
+++ app/src/main/java/pl/dom3k/picipolodelux/MainActivity.java
@@ -0,0 +1,20 @@
+package pl.dom3k.picipolodelux;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onNewGameClick(View view){
+        Intent i = new Intent(this,InsideGameActivity.class);
+        startActivity(i);
+    }
+}
