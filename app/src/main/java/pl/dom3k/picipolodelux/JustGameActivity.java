28e790777fe3d734d64cc78697e077c5139b438a
d0m3k <ja@dom3k.pl>
2016-05-13 14:19:44 +0200

    Waiting room is ready; JustGameActivity needs to be completed, though.

--- app/src/main/java/pl/dom3k/picipolodelux/JustGameActivity.java
+++ app/src/main/java/pl/dom3k/picipolodelux/JustGameActivity.java
@@ -1,7 +1,8 @@
 package pl.dom3k.picipolodelux;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class JustGameActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class JustGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_just_game);
+
+        String gameName = getIntent().getStringExtra("GAMENAME");
+
+        TextView name = (TextView) findViewById(R.id.gamename);
+        if (name != null) {
+            name.setText(gameName);
+        }
+
+
     }
 }
--- app/src/main/java/pl/dom3k/picipolodelux/WaitingRoomActivity.java
+++ app/src/main/java/pl/dom3k/picipolodelux/WaitingRoomActivity.java
@@ -1,13 +1,67 @@
 package pl.dom3k.picipolodelux;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class WaitingRoomActivity extends AppCompatActivity {
+
+    String gameName = null;
+
+    class StateChecker extends AsyncTask<String, String, String> {
+        int count = 0;
+
+        @Override
+        protected String doInBackground(String... params) {
+            String serRes = null;
+            do{
+                SystemClock.sleep(1000);
+                try {
+                    serRes = ServerConnector.currState(gameName,getApplicationContext());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                publishProgress(String.format("Still waiting, for %d seconds...",count++));
+            } while(serRes==null||serRes.split(":")[2].isEmpty());
+
+            publishProgress(String.format("Connected. Playing with %s",serRes.split(":")[4]));
+            SystemClock.sleep(1000);
+            return null;
+        }
+
+        protected void onProgressUpdate(String... progress) {
+            TextView status = (TextView) findViewById(R.id.state);
+            if (status != null) {
+                status.setText(progress[0]);
+            }
+        }
+
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(WaitingRoomActivity.this, JustGameActivity.class);
+            intent.putExtra("GAMENAME",gameName);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting_room);
+
+        gameName = getIntent().getStringExtra("GAMENAME");
+
+        TextView name = (TextView) findViewById(R.id.gamename);
+        if (name != null) {
+            name.setText(gameName);
+        }
+
+        new StateChecker().execute();
     }
 }
